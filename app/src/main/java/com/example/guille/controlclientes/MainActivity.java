e768aa4ee3e88da9aafe47a5ef571397760a9b0e
GuillermoMorales <00011417@uca.edu.sv>
2019-03-14 18:21:48 -0600

    Ultimos toques

--- app/src/main/java/com/example/guille/controlclientes/MainActivity.java
+++ app/src/main/java/com/example/guille/controlclientes/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.guille.controlclientes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv1,tv2;
+    Button btn1,btn2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializar();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String n1 = tv1.getText().toString();
+
+                int numero1 = Integer.parseInt(n1);
+                numero1 = numero1+1;
+                tv1.setText(""+numero1);
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String n2 = tv2.getText().toString();
+
+                int numero2 = Integer.parseInt(n2);
+                numero2 = numero2+1;
+                tv2.setText(""+numero2);
+
+            }
+        });
+    }
+
+    public void inicializar()
+    {
+        tv1 = (TextView)findViewById(R.id.tv1);
+        tv2 = (TextView)findViewById(R.id.tv2);
+        btn1 =(Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
     }
 }
